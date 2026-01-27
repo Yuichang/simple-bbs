29725e814532f11a7d19d8015d6d01cec15b5762
Yuichang <zuka0726@gmail.com>
2026-01-27 16:24:08 +0900

    add del-button

--- static/script.js
+++ static/script.js
@@ -1,9 +1,20 @@
-// 内容の表示を後で実装する
-function check(bodyText){
+// ポストの投稿チェック
+function checkSubmit(bodyText){
     if(window.confirm("この内容で送信してよろしいですか？\n"+bodyText)){
         return true;
     }else{
         window.alert("送信がキャンセルされました。");
+        return false;
+    }
+}
+
+// ポストを削除できるかチェック
+// (一旦削除ボタンの挙動確認。後にDB連携)
+
+function isPostOwner(userId=0, postUserId=0){
+    if(userId === postUserId){
+        return true;
+    }else{
         return false;
     }
 }
